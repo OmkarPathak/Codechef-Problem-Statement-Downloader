0f817929c0f04c5bfa5d51d49bd298b4006413ce
Omkar Pathak <omkarpathak27@gmail.com>
2017-06-28 07:37:55 +0530

    Codechef Problem Statement Downloder

---
+++ CodechefProblemStatementDownloader.py
@@ -0,0 +1,41 @@
+# Author: OMKAR PATHAK
+
+import urllib.request, bs4, re, os, time
+
+problems = ['school', 'easy', 'medium', 'hard', 'challenge', 'extcontest']
+
+for problem in problems:
+    try:
+        os.chdir('..')
+        os.mkdir(problem)
+        if os.path.exists(problem):
+            os.chdir(problem)
+            # web address of codechef website
+            codechefWebsite = 'https://www.codechef.com'
+            # Get the HTML from teh website
+            getHTML = urllib.request.urlopen(codechefWebsite + '/problems/' + problem)
+            # Read the data
+            data = getHTML.read()
+            soup = bs4.BeautifulSoup(data, 'html.parser')
+            check = soup.find_all(class_ = 'content-wrapper')
+
+            result = soup.find_all(href = re.compile('problems/'))
+            for i in range(15, len(result)):
+                checkResult = result[i]['href']
+                try:
+                    getProblem = urllib.request.urlopen(codechefWebsite + checkResult)
+                    dataResult = getProblem.read()
+
+                    soup = bs4.BeautifulSoup(dataResult, 'html.parser')
+                    check = soup.find_all(['p', 'h3', 'ul'])
+                    # print(checkResult)
+                    f = open(checkResult[10:] + '.txt', 'a')
+                    for i in range(7, len(check) - 18):
+                        f.write(check[i].text + '\n')
+                    f.close()
+                except urllib.error.HTTPError:
+                    time.sleep(2)
+        else:
+            pass
+    except FileExistsError:
+        continue

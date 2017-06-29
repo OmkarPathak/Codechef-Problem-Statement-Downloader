e38f5fa67d9f4af0b34f6f81b0a3efa1cae108ba
Omkar Pathak <omkarpathak27@gmail.com>
2017-06-29 08:17:09 +0530

    Added Comments and bug fixes

--- CodechefProblemStatementDownloader.py
+++ CodechefProblemStatementDownloader.py
@@ -8,9 +8,12 @@ problems = ['school', 'easy', 'medium', 'hard', 'challenge', 'extcontest']
 
 for idx, problem in enumerate(problems):
     try:
-        if idx == 1:
+        if idx >= 1:
             os.chdir('..')
+
+        # Create a new directory
         os.mkdir(problem)
+        # If directory exists, go to that directory to save all the files
         if os.path.exists(problem):
             os.chdir(problem)
             # web address of codechef website
@@ -19,24 +22,32 @@ for idx, problem in enumerate(problems):
             getHTML = urllib.request.urlopen(codechefWebsite + '/problems/' + problem)
             # Read the data
             data = getHTML.read()
+            # Parse the HTML data
             soup = bs4.BeautifulSoup(data, 'html.parser')
+            # Find the content-wrapper for all probl statements
             check = soup.find_all(class_ = 'content-wrapper')
 
+            # Find specific href tags that have 'problems/' in them
             result = soup.find_all(href = re.compile('problems/'))
             for i in range(15, len(result)):
                 checkResult = result[i]['href']
                 try:
+                    # Opening each and every problem statements webpage and parsing the data
                     getProblem = urllib.request.urlopen(codechefWebsite + checkResult)
+                    # Read the data
                     dataResult = getProblem.read()
 
                     soup = bs4.BeautifulSoup(dataResult, 'html.parser')
-                    check = soup.find_all(['p', 'h3', 'ul'])
-                    # print(checkResult)
+                    # Find all the elements with HTML tag names
+                    check = soup.find_all(['p', 'h3', 'ul', 'pre'])
+                    # Save every file with the code name
                     f = open(checkResult[10:] + '.txt', 'a')
+                    # Write all the text between those HTML tags in to thhe file just created
                     for i in range(7, len(check) - 18):
                         f.write(check[i].text + '\n')
                     f.close()
                 except urllib.error.HTTPError:
+                    # This exception has to bbe caught, else you might get an error saying Service temporarily unavailable
                     time.sleep(2)
         else:
             pass
